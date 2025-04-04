50a7f21f29727433125b39ea16b53ba0315b9028
harshagopal <harshagopalakrishna1994@gmail.com>
2025-04-04 06:38:03 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,14 @@
+import requests
+
+BOT_TOKEN = "7903820907:AAHEwfUQEZMrwkG-bU8kCFZ0fJOAUTDGUuA"
+CHAT_ID = "@aiappsselfcreation"
+MESSAGE = "Hello, this is an automated message from my Telegram bot!"
+
+def send_message():
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    data = {"chat_id": CHAT_ID, "text": MESSAGE}
+    response = requests.post(url, data=data)
+    return response.json()
+
+if __name__ == "__main__":
+    send_message()
